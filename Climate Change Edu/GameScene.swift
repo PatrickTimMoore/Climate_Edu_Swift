c3af0060a82f0af630bed0ef4ca92cab1583e3a8
Patrick Moore <patrick@brian-moores-mac-pro.local>
2019-05-24 12:44:52 -0500

    Updated Color Pallet, minor bug fixes, added text

--- Climate Change Edu/GameScene.swift
+++ Climate Change Edu/GameScene.swift
@@ -54,7 +54,7 @@ class GameScene: SKScene {
         let path1 = CGMutablePath()
         path1.addLines(between: [p1, p2, p3])
         cweBG.path = path1
-        cweBG.fillColor = SKColor.lightGray
+        cweBG.fillColor = SKColor.white
         cweBG.name = "cwe_space"
         self.addChild(cweBG)
         // Create Combo Area
@@ -65,36 +65,44 @@ class GameScene: SKScene {
         let cwBar: [CGPoint] = [CGPoint(x: p1.x, y: p1.y + (barWidth / 2)), CGPoint(x: p2.x, y: p2.y + (barWidth / 2)), CGPoint(x: p2.x, y: p2.y - (barWidth / 2)), CGPoint(x: p1.x, y: p1.y - (barWidth / 2))]
         path2.addLines(between: [cwBar[0], cwBar[1], cwBar[2], cwBar[3]])
         cwBG.path = path2
-        cwBG.fillColor = SKColor.purple
+        cwBG.fillColor = SKColor.magenta
+        cwBG.strokeColor = SKColor.black
+        cwBG.lineWidth = 4 * ratio
         cwBG.name = "cw_space"
         self.addChild(cwBG)
-        // Create Combo Area
-        weBG = SKShapeNode()
-        weBG.position = CGPoint(x: 0, y: 0)
-        weBG.zPosition = 2
-        let path3 = CGMutablePath()
-        let weBar: [CGPoint] = [CGPoint(x: p1.x + ((barWidth * CGFloat(3.squareRoot())) / 4), y: p1.y + (barWidth / 4)), CGPoint(x: p3.x + ((barWidth * CGFloat(3.squareRoot())) / 4), y: p3.y + (barWidth / 4)), CGPoint(x: p3.x - ((barWidth * CGFloat(3.squareRoot())) / 4), y: p3.y - (barWidth / 4)), CGPoint(x: p1.x - ((barWidth * CGFloat(3.squareRoot())) / 4), y: p1.y - (barWidth / 4))]
-        path3.addLines(between: [weBar[0], weBar[1], weBar[2], weBar[3]])
-        weBG.path = path3
-        weBG.fillColor = SKColor.cyan
-        weBG.name = "we_space"
-        self.addChild(weBG)
         // Create Combo Area
         ceBG = SKShapeNode()
         ceBG.position = CGPoint(x: 0, y: 0)
         ceBG.zPosition = 2
-        let path4 = CGMutablePath()
-        let ceBar: [CGPoint] = [CGPoint(x: p3.x + ((barWidth * CGFloat(3.squareRoot())) / 4), y: p3.y - (barWidth / 4)), CGPoint(x: p2.x + ((barWidth * CGFloat(3.squareRoot())) / 4), y: p2.y - (barWidth / 4)), CGPoint(x: p2.x - ((barWidth * CGFloat(3.squareRoot())) / 4), y: p2.y + (barWidth / 4)), CGPoint(x: p3.x - ((barWidth * CGFloat(3.squareRoot())) / 4), y: p3.y + (barWidth / 4))]
-        path4.addLines(between: [ceBar[0], ceBar[1], ceBar[2], ceBar[3]])
-        ceBG.path = path4
-        ceBG.fillColor = SKColor.yellow
+        let path3 = CGMutablePath()
+        let ceBar: [CGPoint] = [CGPoint(x: p1.x + ((barWidth * CGFloat(3.squareRoot())) / 4), y: p1.y + (barWidth / 4)), CGPoint(x: p3.x + ((barWidth * CGFloat(3.squareRoot())) / 4), y: p3.y + (barWidth / 4)), CGPoint(x: p3.x - ((barWidth * CGFloat(3.squareRoot())) / 4), y: p3.y - (barWidth / 4)), CGPoint(x: p1.x - ((barWidth * CGFloat(3.squareRoot())) / 4), y: p1.y - (barWidth / 4))]
+        path3.addLines(between: [ceBar[0], ceBar[1], ceBar[2], ceBar[3]])
+        ceBG.path = path3
+        ceBG.fillColor = SKColor.cyan
+        ceBG.strokeColor = SKColor.black
+        ceBG.lineWidth = 4 * ratio
         ceBG.name = "ce_space"
         self.addChild(ceBG)
+        // Create Combo Area
+        weBG = SKShapeNode()
+        weBG.position = CGPoint(x: 0, y: 0)
+        weBG.zPosition = 2
+        let path4 = CGMutablePath()
+        let weBar: [CGPoint] = [CGPoint(x: p3.x + ((barWidth * CGFloat(3.squareRoot())) / 4), y: p3.y - (barWidth / 4)), CGPoint(x: p2.x + ((barWidth * CGFloat(3.squareRoot())) / 4), y: p2.y - (barWidth / 4)), CGPoint(x: p2.x - ((barWidth * CGFloat(3.squareRoot())) / 4), y: p2.y + (barWidth / 4)), CGPoint(x: p3.x - ((barWidth * CGFloat(3.squareRoot())) / 4), y: p3.y + (barWidth / 4))]
+        path4.addLines(between: [weBar[0], weBar[1], weBar[2], weBar[3]])
+        weBG.path = path4
+        weBG.fillColor = SKColor.yellow
+        weBG.strokeColor = SKColor.black
+        weBG.lineWidth = 4 * ratio
+        weBG.name = "we_space"
+        self.addChild(weBG)
         // Create Climate Circle
         climateBG = SKShapeNode.init(circleOfRadius: (circleWidth / 2))
         climateBG.position = p1
         climateBG.zPosition = 3
         climateBG.fillColor = SKColor.blue
+        climateBG.strokeColor = SKColor.black
+        climateBG.lineWidth = 4 * ratio
         climateBG.name = "c_space"
         self.addChild(climateBG)
         // Create Weather Circle
@@ -102,6 +110,8 @@ class GameScene: SKScene {
         weatherBG.position = p2
         weatherBG.zPosition = 3
         weatherBG.fillColor = SKColor.red
+        weatherBG.strokeColor = SKColor.black
+        weatherBG.lineWidth = 4 * ratio
         weatherBG.name = "w_space"
         self.addChild(weatherBG)
         // Create Enviroment Circle
@@ -109,7 +119,75 @@ class GameScene: SKScene {
         enviromBG.position = p3
         enviromBG.zPosition = 3
         enviromBG.fillColor = SKColor.green
+        enviromBG.strokeColor = SKColor.black
+        enviromBG.lineWidth = 4 * ratio
         enviromBG.name = "e_space"
         self.addChild(enviromBG)
+        // Create Immobile Circle Labels
+        let cLabel = SKLabelNode(fontNamed: "ArialMT")
+        let wLabel = SKLabelNode(fontNamed: "ArialMT")
+        let eLabel = SKLabelNode(fontNamed: "ArialMT")
+        cLabel.text = "Climate"
+        wLabel.text = "Weather"
+        eLabel.text = "Enviroment"
+        cLabel.fontSize = 50 * ratio
+        wLabel.fontSize = 50 * ratio
+        eLabel.fontSize = 50 * ratio
+        cLabel.position = CGPoint(x: climateBG.frame.midX, y: climateBG.frame.midY - (cLabel.fontSize / 2))
+        wLabel.position = CGPoint(x: weatherBG.frame.midX, y: weatherBG.frame.midY - (cLabel.fontSize / 2))
+        eLabel.position = CGPoint(x: enviromBG.frame.midX, y: enviromBG.frame.midY - (cLabel.fontSize / 2))
+        cLabel.zPosition = 4
+        wLabel.zPosition = 4
+        eLabel.zPosition = 4
+        self.addChild(cLabel)
+        self.addChild(wLabel)
+        self.addChild(eLabel)
+        // Create Immobile Bar Labels
+        let cwLabel1 = SKLabelNode(fontNamed: "ArialMT")
+        let cwLabel2 = SKLabelNode(fontNamed: "ArialMT")
+        cwLabel1.text = "Climate and"
+        cwLabel2.text = "Weather"
+        cwLabel1.fontSize = 30 * ratio
+        cwLabel2.fontSize = 30 * ratio
+        cwLabel1.position = CGPoint(x: cwBG.frame.midX, y: cwBG.frame.midY + (cwLabel1.fontSize * 0.2))
+        cwLabel2.position = CGPoint(x: cwBG.frame.midX, y: cwBG.frame.midY - (cwLabel2.fontSize * 1.2))
+        cwLabel1.zPosition = 4
+        cwLabel2.zPosition = 4
+        cwLabel1.fontColor = SKColor.black
+        cwLabel2.fontColor = SKColor.black
+        self.addChild(cwLabel1)
+        self.addChild(cwLabel2)
+        let ceLabel1 = SKLabelNode(fontNamed: "ArialMT")
+        let ceLabel2 = SKLabelNode(fontNamed: "ArialMT")
+        ceLabel1.text = "Climate and"
+        ceLabel2.text = "Enviroment"
+        ceLabel1.fontSize = 30 * ratio
+        ceLabel2.fontSize = 30 * ratio
+        ceLabel1.position = CGPoint(x: ceBG.frame.midX + (ceLabel1.fontSize * 0.2 * CGFloat(3).squareRoot()), y: ceBG.frame.midY + (ceLabel1.fontSize * 0.2))
+        ceLabel2.position = CGPoint(x: ceBG.frame.midX - (ceLabel2.fontSize * 0.5 * CGFloat(3).squareRoot()), y: ceBG.frame.midY - (ceLabel2.fontSize * 0.5))
+        ceLabel1.zPosition = 4
+        ceLabel2.zPosition = 4
+        ceLabel1.fontColor = SKColor.black
+        ceLabel2.fontColor = SKColor.black
+        ceLabel1.zRotation = -60 * CGFloat.pi / 180
+        ceLabel2.zRotation = -60 * CGFloat.pi / 180
+        self.addChild(ceLabel1)
+        self.addChild(ceLabel2)
+        let weLabel1 = SKLabelNode(fontNamed: "ArialMT")
+        let weLabel2 = SKLabelNode(fontNamed: "ArialMT")
+        weLabel1.text = "Enviroment"
+        weLabel2.text = "and Weather"
+        weLabel1.fontSize = 30 * ratio
+        weLabel2.fontSize = 30 * ratio
+        weLabel1.position = CGPoint(x: weBG.frame.midX - (weLabel1.fontSize * 0.2 * CGFloat(3).squareRoot()), y: weBG.frame.midY + (weLabel1.fontSize * 0.2))
+        weLabel2.position = CGPoint(x: weBG.frame.midX + (weLabel2.fontSize * 0.5 * CGFloat(3).squareRoot()), y: weBG.frame.midY - (weLabel2.fontSize * 0.5))
+        weLabel1.zPosition = 4
+        weLabel2.zPosition = 4
+        weLabel1.fontColor = SKColor.black
+        weLabel2.fontColor = SKColor.black
+        weLabel1.zRotation = 60 * CGFloat.pi / 180
+        weLabel2.zRotation = 60 * CGFloat.pi / 180
+        self.addChild(weLabel1)
+        self.addChild(weLabel2)
     }
 }
